34632c845b526cd44c59605afc67c6ab73217770
deepakjayaprakash <deepakjpkodlekere@gmail.com>
2021-05-13 12:40:51 +0530

    added hystrix

--- src/main/java/com/example/hystrix/client/demo/AppController.java
+++ src/main/java/com/example/hystrix/client/demo/AppController.java
@@ -1,16 +1,34 @@
 package com.example.hystrix.client.demo;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping
 public class AppController {
 
+    Logger logger = LoggerFactory.getLogger(AppController.class);
+
+//    @Autowired
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @HystrixCommand(fallbackMethod = "getDefaultResponse")
     @RequestMapping(value = "/client", method = RequestMethod.GET)
     public ResponseDTO client() {
         ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO = restTemplate.getForObject("localhost:8081/getSimpleResponse", ResponseDTO.class);
+        return responseDTO;
+    }
+
+    private ResponseDTO getDefaultResponse() {
+        logger.info("in default method");
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setBody("defaultResponse");
         return responseDTO;
     }
 }
--- src/main/java/com/example/hystrix/client/demo/HystrixClientApplication.java
+++ src/main/java/com/example/hystrix/client/demo/HystrixClientApplication.java
@@ -2,12 +2,14 @@ package com.example.hystrix.client.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class HystrixClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HystrixClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixClientApplication.class, args);
+    }
 
 }
